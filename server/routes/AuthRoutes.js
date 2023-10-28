193b4ab95b87492dbb37da4e1af2a4b6683cad8b
Sameer Gorrepati <64128604+sameerg1810@users.noreply.github.com>
2023-10-28 13:40:00 +0530

    Delete server directory

--- server/routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-import { SignUpController, getMyUsers } from '../controllers/AuthAdminController.js';
-import { PostCo_Ordinates, userSignUp } from '../controllers/AuthUserController.js';
-const AuthRouter=Router()
-AuthRouter.post('/admin/signup',SignUpController)
-AuthRouter.post('/user/signup',userSignUp)
-AuthRouter.get('/admin/:id',getMyUsers)
-AuthRouter.post('/user/cords/:id',PostCo_Ordinates)
-export default AuthRouter
